02391d25092afaa64be1039343c005158eb75a28
ekremgunes <github1357@gmail.com>
2023-02-11 13:58:14 +0300

    pushed

---
+++ DynamicViewAlert/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DynamicViewAlert.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ViewAlertApp.Extensions;
+
+namespace DynamicViewAlert.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            TempData["alerts"] = this.ViewAlert(AlertType.Success,"gunesekrem.com success");
+            
+            TempData["alerts"] += this.ViewAlert(AlertType.Info,"info message");
+            
+            TempData["alerts"] += this.ViewAlert(AlertType.Warning,"warnin message");
+            
+            TempData["alerts"] += this.ViewAlert(AlertType.Error,"error message");
+
+            //gunesekrem.com
+            
+            return View();
+        }
+
+    }
+}
---
+++ DynamicViewAlert/Extensions/ControllerExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewAlertApp.Extensions
+{
+    public static class ControllerExtensions
+    {
+        //scripted by gunesekrem.com
+        public static string ViewAlert(this Controller controller, AlertType alert = AlertType.Info, string alertMessage = "Info")
+        {
+            if (alert == AlertType.Success)
+            {
+                return $"<span class='successAlert'>{alertMessage}</span>";
+            }
+            else if (alert == AlertType.Warning)
+            {
+                return $"<span class='warningAlert'>{alertMessage}</span>";
+            }
+            else if (alert == AlertType.Error)
+            {
+                return $"<span class='errorAlert'>{alertMessage}</span>";
+            }
+            else
+            {
+                return $"<span class='infoAlert'>{alertMessage}</span>";
+            }
+        }
+    }
+
+    public enum AlertType
+    {
+        Success = 1,
+        Error = 2,
+        Info = 3,
+        Warning = 4
+    }
+}
